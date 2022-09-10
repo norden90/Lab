1e36a308619c8cfdc848ac7ec7b3b9dd4a82ca5a
norden90 <nordfeldt.andreas@gmail.com>
2022-09-10 13:38:31 +0200

    test

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Lovisa är bäst!");
+Console.WriteLine("Min finaste Lovisa");
